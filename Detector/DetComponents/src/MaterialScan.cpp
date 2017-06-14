3459acfb7026d225dc5d4079995f102121bf658e
CoralieNeubueser <coralie.neubuser@cern.ch>
2017-06-14 13:05:11 +0200

    material scan corrected & HCAL EB length increased by 50cm

--- Detector/DetComponents/src/MaterialScan.cpp
+++ Detector/DetComponents/src/MaterialScan.cpp
@@ -66,7 +66,7 @@ StatusCode MaterialScan::initialize() {
   std::array<Double_t, 3> pos = {0, 0, 0};
   std::array<Double_t, 3> dir = {0, 0, 0};
   TVector3 vec(0, 0, 0);
-  for (eta = -m_etaMax; eta < m_etaMax; eta += m_etaBinning) {
+  for (eta = (-m_etaMax+m_etaBinning/2); eta < (m_etaMax+m_etaBinning/2); eta += m_etaBinning) {
       for (int i = 0; i < m_nPhiTrials; ++i) {
       phi = m_flatPhiDist();
       nX0->clear();
@@ -78,18 +78,18 @@ StatusCode MaterialScan::initialize() {
       for (int iPhi = 0; iPhi < m_nPhiTrials; ++iPhi) {
           phi = m_flatPhiDist();
           //phi = 0;
-        vec.SetPtEtaPhi(1, eta, phi);
-        auto n = vec.Unit();
-        dir = {n.X(), n.Y(), n.Z()};
-        double distance = worldVol->DistFromInside(pos.data(), dir.data());
-
-        DDSurfaces::Vector3D end(dir[0] * distance, dir[1] * distance, dir[2] * distance);
-        debug() << "Calculating material between 0 and (" << end.x() << ", " << end.y() << ", " << end.z()
-                << ") <=> eta = " << eta << ", phi =  " << phi  <<  endmsg;
-        const DD4hep::DDRec::MaterialVec& materials = matMgr.materialsBetween(beginning, end);
-        for (unsigned i = 0, n = materials.size(); i < n; ++i) {
-          phiAveragedMaterialsBetween[materials[i].first] += materials[i].second / static_cast<double>(m_nPhiTrials);
-      }
+	  vec.SetPtEtaPhi(1, eta, phi);
+	  auto n = vec.Unit();
+	  dir = {n.X(), n.Y(), n.Z()};
+	  double distance = worldVol->DistFromInside(pos.data(), dir.data());
+	  
+	  DDSurfaces::Vector3D end(dir[0] * distance, dir[1] * distance, dir[2] * distance);
+	  debug() << "Calculating material between 0 and (" << end.x() << ", " << end.y() << ", " << end.z()
+		  << ") <=> eta = " << eta << ", phi =  " << phi  <<  endmsg;
+	  const DD4hep::DDRec::MaterialVec& materials = matMgr.materialsBetween(beginning, end);
+	  for (unsigned i = 0, n = materials.size(); i < n; ++i) {
+	    phiAveragedMaterialsBetween[materials[i].first] += materials[i].second / static_cast<double>(m_nPhiTrials);
+	  }
       }
       nMaterials = phiAveragedMaterialsBetween.size();
       for (auto matpair: phiAveragedMaterialsBetween) {
